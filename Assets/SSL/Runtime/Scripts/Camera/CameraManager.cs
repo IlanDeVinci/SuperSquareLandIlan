5707eeea12325e22a2ae470dd3b9e53709e6dcf1
IlanDeVinci <ilan.maouchi@gmail.com>
2024-04-26 11:08:39 +0200

    commit

--- Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -10,23 +10,73 @@ public class CameraManager : MonoBehaviour
     [Header("Profile System")]
     [SerializeField] private CameraProfile _defaultCameraProfile;
     private CameraProfile _currentCameraProfile;
+    private float _profileTransitionTimer = 0f;
+    private float _profileTransitionDuration = 0f;
+    private Vector3 _profileTransitionStartPosition;
+    private float _profileTransitionStartSize = 0f;
 
+    private float _CalculateProfileTransitionCameraSize(float endSize)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        percent = Mathf.Clamp01(percent);
+        float startSize = _profileTransitionStartSize;
+        return Mathf.Lerp(startSize, endSize, percent);
+    }
+    private Vector3 _CalculateProfileTransitionPosition(Vector3 destination)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        percent = Mathf.Clamp01(percent);
+        Vector3 origine = _profileTransitionStartPosition;
+        return Vector3.Lerp(origine, destination, percent);
+    }
+
+
+    private void _PlayProfileTransition(CameraProfileTransition transition)
+    {
+        _profileTransitionStartPosition = _camera.transform.position;
+        _profileTransitionStartSize = _camera.orthographicSize;
+        _profileTransitionTimer = 0f;
+        _profileTransitionDuration = transition.duration;
+    }
+
+    private bool _IsPlayingProfileTransition()
+    {
+        return _profileTransitionTimer < _profileTransitionDuration;
+    }
     private void Update()
     {
-        SetCameraPosition(_currentCameraProfile.Position);
-        SetCameraSize(_currentCameraProfile.CameraSize);
-
+        if(_IsPlayingProfileTransition())
+        {
+            _profileTransitionTimer += Time.deltaTime;
+            Vector3 transitionPosition = _CalculateProfileTransitionPosition(_currentCameraProfile.Position);
+            SetCameraPosition(transitionPosition);
+            float transitionSize = _CalculateProfileTransitionCameraSize(_currentCameraProfile.CameraSize);
+            SetCameraSize(transitionSize);
+        }
+        else
+        {
+            SetCameraPosition(_currentCameraProfile.Position);
+            SetCameraSize(_currentCameraProfile.CameraSize);
+        }
     }
 
-    public void EnterProfile(CameraProfile cameraProfile)
+    public void EnterProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
         _currentCameraProfile = cameraProfile;
+        if (transition != null)
+        {
+            _PlayProfileTransition(transition);
+        }
     }
 
-    public void ExitProfile(CameraProfile cameraProfile)
+    public void ExitProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
         if (_currentCameraProfile != cameraProfile) return;
         _currentCameraProfile = _defaultCameraProfile;
+        if (transition != null)
+        {
+            _PlayProfileTransition(transition);
+        }
     }
 
     private void Awake()
